53a35244ea92d6ef831b74eb8a2974c758551af0
Tokyo Choshi <tokyochoshi@gmail.com>
2022-11-09 18:49:17 +0200

    added project structure

---
+++ src/main/java/com/reactive/webflux/ReactiveWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebfluxApplication.class, args);
+	}
+
+}
